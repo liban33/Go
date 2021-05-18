38c5595cdff6ec93fca8cfa97b01dfbf16d0f1b6
Liban33 <liban_jama@hotmail.com>
2021-05-18 13:14:38 +0100

    added a fresh config file

--- web.go
+++ web.go
@@ -6,7 +6,8 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>welcome to my page1</h1>")
+	fmt.Print("someone visited the")
+	fmt.Fprint(w, "<h1>welcome to my new page1</h1>")
 
 }
 
