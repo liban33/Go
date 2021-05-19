01c81f92343a6617bc915e2e40912d7b0379e75f
Liban33 <liban_jama@hotmail.com>
2021-05-19 10:33:34 +0100

    added a 404 page

--- web.go
+++ web.go
@@ -7,13 +7,16 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/"{
+	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Welcome to my website</h1>")
-	} else if r.URL.Path == "/contact"{
+	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "Get in touch, send an email to <a href=\"mailto:liban_jama@hotmail.com\">liban_jama@hotmail.com</>.")
-		}
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>we could not find your page</h1><p>please email us if you keep being sent to an invalid page.</p>")
+	}
 }
-	
+
 func main() {
 	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":7000", nil)
