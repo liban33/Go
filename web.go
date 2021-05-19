b905ede0eebd3322ac63336722ef5b33a542b2ec
Liban33 <liban_jama@hotmail.com>
2021-05-19 12:48:18 +0100

    added an FAQ page.

--- web.go
+++ web.go
@@ -28,11 +28,16 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-Type", "text/Html")
 	fmt.Fprint(w, "Get in touch, send an email to <a href=\"mailto:liban_jama@hotmail.com\">liban_jama@hotmail.com</>.")
 }
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "text/html")
+	fmt.Fprint(w, "<h1>frequently asked Questions?</h1><p> Here is a list of questions that our users commonly ask.</p>")
+}
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	//http.HandleFunc("/contact", handlerFunc)
 	http.ListenAndServe(":7000", r)
 
